970e48c6bb219b5a78722a19283e094845994f44
AtharvBarge24 <148458893+AtharvBarge24@users.noreply.github.com>
2023-11-11 00:10:35 +0530

    Update Year.java
    enter your fav year

--- Year.java
+++ Year.java
@@ -4,7 +4,7 @@ public class Year {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
     int y;
-        System.out.println("Enter a year:  ");
+        System.out.println("Enter your fav year:  ");
         y= sc.nextInt();
 
         if(y%100==0   &&  y%400==0 ||
