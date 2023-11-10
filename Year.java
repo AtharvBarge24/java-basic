39892ea873bca8036f13a7da7104662dd557c47e
atharv <atharvbarge24>
2023-11-10 23:59:00 +0530

    first commit

---
+++ Year.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Year {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+    int y;
+        System.out.println("Enter a year:  ");
+        y= sc.nextInt();
+
+        if(y%100==0   &&  y%400==0 ||
+           y%100!=0   &&  y%4==0)
+        {
+            System.out.println("year is leap");
+
+        }
+        else
+        {
+            System.out.println("year is not leap");
+        }
+    }
+}
